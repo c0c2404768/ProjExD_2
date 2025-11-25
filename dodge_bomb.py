44f3a596bcea063c1d22085da50fab3fa5a50965
c0c24047 <c0c2404768@edu.teu.ac.jp>
2025-11-25 16:38:27 +0900

    演習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,30 +2,52 @@ import os
 import random
 import sys
 import pygame as pg
+import time
 
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
-    pg.K_UP:    (0, -5),
-    pg.K_DOWN:  (0, +5),
-    pg.K_LEFT:  (-5, 0),
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
-    """引数: こうかとんRect または 爆弾のRect
-    戻り値: 横方向、縦方向のはみ出し判定結果
-    （画面内ならTrue,画面外ならFalse）
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：判定結果タプル（横方向，縦方向）
+    画面内ならTrue，画面外ならFalse
     """
     yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:#横方向のはみ出しチェック
+    if rct.left < 0 or WIDTH <rct.right:  # 横方向のはみ出しチェック
         yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:#縦方向のはみ出しチェック
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向のはみ出しチェック
         tate = False
     return yoko, tate
 
+def gameover(screen: pg.Surface) -> None:
+    """ゲームオーバー時の処理を行う関数"""
+    gg_img = pg.Surface((WIDTH, HEIGHT))  # 空のSurface
+    gg_rct = gg_img.get_rect()  # ゲームオーバーRect
+    pg.draw.rect(gg_img, (0, 0, 0), pg.Rect(0,0,WIDTH, HEIGHT))  # 黒い四角形を描画
+    gg_img.set_alpha((200))  #透明度 の設定
+    fonto = pg.font.Font(None, 80)
+    txt = fonto.render("Game Over", True, (255, 255, 255))
+    rct=txt.get_rect()
+    rct.center=WIDTH/2, HEIGHT/2
+    gg_img.blit(txt,rct)
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 2.0)
+    kk_rct=kk_img.get_rect()
+    kk_rct.center=WIDTH/2-250, HEIGHT/2
+    gg_img.blit(kk_img, kk_rct)
+    kk_rct.center=WIDTH/2+250, HEIGHT/2
+    gg_img.blit(kk_img, kk_rct)
+    screen.blit(gg_img, [0, 0])
+    pg.display.update()
+    time.sleep(5)
 
 
 def main():
@@ -35,13 +57,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bb_img = pg.Surface((20, 20))#空のSurface
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)#半径10の赤い円を描画
-    bb_img.set_colorkey((0, 0, 0))#黒色を透明に設定
-    bb_rct = bb_img.get_rect()#爆弾Rect
-    bb_rct.centerx = random.randint(0, WIDTH)#爆弾横座標
-    bb_rct.centery = random.randint(0, WIDTH)#爆弾縦座標
-    vx, vy = +5, +5#爆弾速度
+    bb_img = pg.Surface((20, 20))  # 空のSurface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 半径10の赤い円を描画
+    bb_img.set_colorkey((0, 0, 0))  # 黒色を透過色に設定
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾横座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾縦座標
+    vx, vy = +5, +5  # 爆弾の横速度，縦速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -49,10 +71,10 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
-        if kk_rct.colliderect(bb_rct):#こうかとんRectと爆弾Rectが接触したら
-            print("ゲームオーバー")
+        if kk_rct.colliderect(bb_rct):  # こうかとんと爆弾が衝突したら
+            gameover(screen)
+            #print("ゲームオーバー")
             return
-            
 
         screen.blit(bg_img, [0, 0]) 
 
@@ -68,20 +90,19 @@ def main():
         #     sum_mv[0] += 5
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0]#横移動量
-                sum_mv[1] += mv[1]#縦移動量
+                sum_mv[0] += mv[0]  # 横方向の移動量
+                sum_mv[1] += mv[1]  # 縦方向の移動量
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True, True):#画面外なら      
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])#移動をなかったことにする
+        if check_bound(kk_rct) != (True, True):  # 画面外なら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 移動をなかったことにする
         screen.blit(kk_img, kk_rct)
         yoko, tate = check_bound(bb_rct)
-        if not yoko:
+        if not yoko:  # 横方向にはみ出ていたら
             vx *= -1
-        if not tate:
+        if not tate:  # 縦方向にはみ出ていたら
             vy *= -1
         bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
-
         pg.display.update()
         tmr += 1
         clock.tick(50)
@@ -89,6 +110,6 @@ def main():
 
 if __name__ == "__main__":
     pg.init()
-    sys.exit()
+    main()
     pg.quit()
     sys.exit()
